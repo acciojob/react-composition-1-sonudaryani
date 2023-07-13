c0d6bfb3e617a622fbbc2e23fe0d97648b090345
sonudaryani <sonudaryani4@gmail.com>
2023-07-13 23:23:06 +0530

    Update

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 
-const Tabs = ({ tabs }) => {
+
+const tabs = [
+    { title: 'Tab 1', content: 'This is the content for Tab 1.' },
+    { title: 'Tab 2', content: 'This is the content for Tab 2.' },
+    { title: 'Tab 3', content: 'This is the content for Tab 3.' },
+];
+
+const Tabs = ()=>{
   const [activeTab, setActiveTab] = useState(0);
 
   const handleClick = (index) => {
@@ -11,6 +18,7 @@ const Tabs = ({ tabs }) => {
     <div>
       <ul>
         {tabs.map((tab, index) => (
+            //each tab it add into the list with handleClick function
           <li onClick={() => handleClick(index)}>{tab.title}</li>
         ))}
       </ul>
