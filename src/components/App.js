7535f33cea800e19ac8c5a4bcce8243545ad8254
sonudaryani <sonudaryani4@gmail.com>
2023-07-13 23:05:44 +0530

    UPDATE

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ const App = () => {
     { title: 'Tab 1', content: 'This is the content for Tab 1.' },
     { title: 'Tab 2', content: 'This is the content for Tab 2.' },
     { title: 'Tab 3', content: 'This is the content for Tab 3.' },
-  ];
+];
 
   return <Tabs tabs={tabs} />;
 };
